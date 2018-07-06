cb56a6821b39b1dc94b1f61fad9ca2a8376e6936
Danny Peng <dannyw.peng@gmail.com>
2018-07-06 00:24:14 -0700

    updated top k frequent solution

--- topKFrequentWords.rb
+++ topKFrequentWords.rb
@@ -1,19 +1,11 @@
 def top_k_frequent(words, k)
-    ans = []
-    counter = 0
-    hash = Hash.new(0)
+  hash = Hash.new(0)
 
-    words.each do |word|
-        hash[word] += 1
-    end
+  words.each do |word|
+      hash[word] += 1
+  end
 
-    sorted = hash.sort_by {|k, v| v }.to_h
-    sorted_words = sorted.keys.reverse
-    i = 0
-    while counter < k
-        ans << sorted_words[i]
-        counter += 1
-        i += 1
-    end
-    ans
+  sorted = hash.sort_by {|k, v| v }.to_h
+  sorted_words = sorted.keys.reverse.take(k)
+  sorted_words
 end
