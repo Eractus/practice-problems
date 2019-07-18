bf1708b4d5066a1732f744aa64f08ec9bd77caff
Danny Peng <dannyw.peng@gmail.com>
2019-07-17 17:06:19 -0700

    optimized solution to not use an extra matrix and refactored to improve performance by removing redundant iterations in for loop

--- unique_paths_ii.js
+++ unique_paths_ii.js
@@ -1,51 +1,45 @@
 var uniquePathsWithObstacles = function(obstacleGrid) {
-    if (obstacleGrid[0][0] === 1) {
-        return 0;
+  if (obstacleGrid[0][0] === 1) {
+    return 0;
+  }
+
+  if (obstacleGrid.length === 1 && obstacleGrid[0].length === 1) {
+    return 1;
+  }
+
+  let finish = [obstacleGrid.length - 1, obstacleGrid[0].length - 1];
+
+  obstacleGrid[0][0] = 1;
+
+  let i = 0;
+  for (let j = 1; j <= finish[1]; j++) {
+    if (obstacleGrid[i][j] === 1) {
+      obstacleGrid[i][j] = 0;
+    } else if (obstacleGrid[i][j] === 0 && obstacleGrid[i][j - 1] !== 0) {
+      obstacleGrid[i][j] = 1;
     }
+  }
 
-    if (obstacleGrid.length === 1 && obstacleGrid[0].length === 1) {
-        return 1;
+  let l = 0;
+  for (let k = 1; k <= finish[0]; k++) {
+    if (obstacleGrid[k][l] === 1) {
+      obstacleGrid[k][l] = 0;
+    } else if (obstacleGrid[k][l] === 0 && obstacleGrid[k - 1][l] !== 0) {
+      obstacleGrid[k][l] = 1;
     }
+  }
 
-    let finish = [obstacleGrid.length - 1, obstacleGrid[0].length - 1];
-    let grid = [];
-    for (let i=0; i<=finish[0]; i++) {
-        let row = [];
-        for (let j=0; j<=finish[1]; j++) {
-            row.push(0);
-        }
-        grid.push(row);
+  for (let m = 1; m <= finish[0]; m++) {
+    for (let n = 1; n <= finish[1]; n++) {
+      if (obstacleGrid[m][n] === 1) {
+        obstacleGrid[m][n] = 0;
+      } else if (obstacleGrid[m][n] === 0) {
+        let pathsFromTop = obstacleGrid[m - 1][n];
+        let pathsFromLeft = obstacleGrid[m][n - 1];
+        obstacleGrid[m][n] = pathsFromTop + pathsFromLeft;
+      }
     }
+  }
 
-    grid[0][0] = 1;
-
-    for (let i=0; i<=finish[0]; i++) {
-        for (let j=0; j<=finish[1]; j++) {
-            if (i === 0 & j === 0) {
-                continue;
-            }
-
-            if (i === 0) {
-                if (obstacleGrid[i][j] === 0 && grid[i][j-1] === 1) {
-                    grid[i][j] = 1;
-                }
-            } else if (j === 0) {
-                if (obstacleGrid[i][j] === 0 && grid[i-1][j] === 1) {
-                    grid[i][j] = 1;
-                }
-            }
-        }
-    }
-
-    for (let i=1; i<=finish[0]; i++) {
-        for (let j=1; j<=finish[1]; j++) {
-            if (obstacleGrid[i][j] === 0) {
-                let pathsFromTop = grid[i-1][j];
-                let pathsFromLeft = grid[i][j - 1];
-                grid[i][j] = pathsFromTop + pathsFromLeft;
-            }
-        }
-    }
-
-    return grid[finish[0]][finish[1]];
+  return obstacleGrid[finish[0]][finish[1]];
 };
